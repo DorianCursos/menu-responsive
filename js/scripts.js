50dfa748c5fc6fd6834bea3fc1a14941225dfe2b
Dorian <doriancursos@gmail.com>
2024-04-15 18:06:43 +0200

    Updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Localizamos los elementos del HTML a través de su ID
 const buttonMenuElement = document.getElementById('button-menu');
 const menuElement = document.getElementById('menu');
 
@@ -6,11 +7,16 @@ const menuElement = document.getElementById('menu');
 // classList.remove
 
 function handleMenu() {
+  // Preguntamos si el menu contiene la clase show
   if (menuElement.classList.contains('show')) {
+    // Si contiene la clase show, se la quitamos.
     menuElement.classList.remove('show');
   } else {
+    // Si no contiene la clase show, se la ponemos
     menuElement.classList.add('show');
   }
 }
 
+// Al elemento que nos interese le añadimos un evento de escucha.
+// Escucha cuando recibas un click y llama a la función handleMenu
 buttonMenuElement.addEventListener('click', handleMenu);
